f933befb04b9a67191ddcd9917644e6553bb6db4
Yohan Abraham <116612238+Yohan-Abraham@users.noreply.github.com>
2026-09-19 13:07:37 -0700

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+       let map = new Map();
+
+
+
+       for(let i = 0; i < nums.length; i++){
+            const required = target - nums[i];
+
+            if(map.has(required) && map.get(required) != i){
+                return [i, map.get(required)];
+            }
+            map.set(nums[i], i);
+
+       }
+       return false
+    }
+}
